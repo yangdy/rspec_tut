f8bdaa58bd5050057e9e5e3d30c6d7221df8f799
yangdy <anvilhorn@163.com>
2014-10-10 13:06:02 +0800

    refactory rspec

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -3,23 +3,18 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
 		describe "#start" do
-
+			before(:each) do
+				@output = double('output').as_null_object
+				@game = Game.new(@output)
+			end
 
 			it "sends a welcome message" do
-				output = double('output').as_null_object
-				game = Game.new(output)
-
-				expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-				#output.should_receive(:puts).with('Welcome to Codebreaker!')
-				game.start
+				expect(@output).to receive(:puts).with('Welcome to Codebreaker!')
+				@game.start
 			end
 			it "prompts for the first guess" do
-				output = double('output').as_null_object
-				game = Game.new(output)
-
-				expect(output).to receive(:puts).with('Enter guess:')
-				#output.should_receive(:puts).with('Entry guess:')
-				game.start
+				expect(@output).to receive(:puts).with('Enter guess:')
+				@game.start
 			end
 		end
   end
